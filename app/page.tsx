f61b11488845e4d091b929badf89e36b5c8661a1
sumamakhan7611 <khansumama87111@gmail.com>
2025-01-15 19:49:47 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+import Code from "@/components/Code"
+export default function Home() {
+  return (
+   <main className="h-full flex items-center justify-center flex-col gap-6">
+      <h1 className="text-3xl font-black"> Code share</h1>
+      <Code/>
+   </main>
+  )
+}
